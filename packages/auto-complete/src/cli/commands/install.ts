5a79ff47c0a0fe3b0773a153149d2a336e133894
mmolisani <mmolisani@bloomberg.net>
2026-01-14 08:08:24 -0500

    feat: unified autocomplete entrypoint
    Signed-off-by: mmolisani <mmolisani@bloomberg.net>

---
+++ packages/auto-complete/src/cli/commands/install.ts
@@ -0,0 +1,22 @@
+// Copyright 2024 Bloomberg Finance L.P.
+// Distributed under the terms of the Apache 2.0 license.
+import shells from "../../shells";
+import type { Shell } from "../../types";
+import type { StricliAutoCompleteContext } from "../context";
+import type { ShellAutoCompleteCommands } from "../spec";
+
+export default async function (
+    this: StricliAutoCompleteContext,
+    flags: ShellAutoCompleteCommands,
+    targetCommand: string,
+): Promise<void> {
+    for (const [shell, autocompleteCommand] of Object.entries(flags)) {
+        const shellManager = await shells[shell as Shell].getCommandManager(this);
+        const message = await shellManager.install(targetCommand, autocompleteCommand);
+        this.process.stdout.write(`Installed autocomplete support for ${targetCommand} in ${shell}.\n`);
+        /* v8 ignore else -- @preserve */
+        if (message) {
+            this.process.stdout.write(`${message}\n`);
+        }
+    }
+}
